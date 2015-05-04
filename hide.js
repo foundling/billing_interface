7f507f43e8ae8aae73aee5b6d42baa5b20771af0
Alex Ramsdell <alexramsdell@gmail.com>
2015-05-04 11:02:38 -0400

    about to redo css

--- hide.js
+++ hide.js
@@ -1,19 +1,21 @@
-var $isotope_checkbox = $('input[name=isotope-only]');
+var $isotope_checkbox = $('.isotope-heading');
 var $isotope_block =  $('.block-isotope');
-var $radiotracer_checkbox = $('input[name=radiotracer]');
+var $radiotracer_checkbox = $('.radiotracer-heading');
 var $radiotracer_block = $('.block-radiotracer');
-var $other_checkbox = $('input[name=other]');
+var $other_checkbox = $('.other-heading');
 var $other_block = $('.block-other');
 
 
-$isotope_checkbox.change( function() {
+
+
+$isotope_checkbox.on('click', function() {
 	$isotope_block.toggleClass('hidden');
 });
 
-$radiotracer_checkbox.change( function() {
+$radiotracer_checkbox.on('click', function() {
 	$radiotracer_block.toggleClass('hidden');
 });
 
-$other_checkbox.change( function() {
+$other_checkbox.on('click', function() {
 	$other_block.toggleClass('hidden');
 });
