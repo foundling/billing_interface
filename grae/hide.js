b04399c1cddd3c7cf461d929d484fe666f7e7323
Alex Ramsdell <alexramsdell@gmail.com>
2015-05-05 13:14:34 -0400

    some progress on grae's form

--- grae/hide.js
+++ grae/hide.js
@@ -1,21 +1,61 @@
-var $isotope_checkbox = $('.isotope-header');
-var $isotope_block =  $('.isotope-block');
-var $radiotracer_checkbox = $('.radiotracer-header');
-var $radiotracer_block = $('.radiotracer-block');
-var $other_checkbox = $('.other-header');
-var $other_block = $('.other-block');
+var $subject_block =  $('.subject-block');
+var $subject_header =  $('.subject-header');
 
 
+/*$subject_header.on('click', function() {
+	$subject_block.toggleClass('hidden');
+});*/
 
 
-$isotope_checkbox.on('click', function() {
-	$isotope_block.toggleClass('hidden');
+var $subject_type_selector = $('#subject-type-selector');
+
+$subject_type_selector.on('change', function() {
+	if (this.value === 'animal')
+	{
+		// cache these
+		$('.human-subject-types').hide();
+		$('.human-details').hide();
+
+		$('.phantom-subject-description').hide();
+
+		$('.animal-subject-types').show();
+		$('.animal-details').show();
+	}
+
+	else if (this.value === 'human')
+	{
+		$('.human-subject-types').show();
+		$('.human-details').show();
+
+		$('.phantom-subject-description').hide();
+
+		$('.animal-subject-types').hide();
+		$('.animal-details').hide();
+	}
+
+	else if (this.value === 'phantom')
+	{
+		$('.human-subject-types').hide();
+		$('.human-details').hide();
+
+		$('.phantom-subject-description').show();
+
+		$('.animal-subject-types').hide();
+		$('.animal-details').hide();
+	}
 });
 
-$radiotracer_checkbox.on('click', function() {
-	$radiotracer_block.toggleClass('hidden');
-});
+var $blood_analysis_type = $("select[name=blood-analysis-type]");
+var $arterial_sub_menu = $('.arterial-block');
+var $venous_sub_menu = $('.venous-block');
 
-$other_checkbox.on('click', function() {
-	$other_block.toggleClass('hidden');
+$blood_analysis_type.on('change', function() {
+	if (this.value === 'arterial') {
+		$arterial_sub_menu.show();
+		$venous_sub_menu.hide();
+	}
+	else if (this.value === 'venous') {
+		$arterial_sub_menu.hide();
+		$venous_sub_menu.show();
+	}
 });
