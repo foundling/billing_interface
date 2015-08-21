70fe1c20bdbb30df5f53fbeff296c1fbdf51485f
root <root@jukebox.nmr.mgh.harvard.edu>
2015-08-21 19:34:17 -0400

    now validate.php sends back EXISTS, NOEXISTS and CLOSED

--- scripts/validateProjectCode.php
+++ scripts/validateProjectCode.php
@@ -4,15 +4,33 @@ include("../db/dbConfig.test.php");
 $conn = mysql_connect($dbHost,$dbUser,$dbPass) or die("Connect Error: " . mysql_error());
 mysql_select_db("acct",$conn);
 $proj_code = mysql_real_escape_string($_GET['code']);
-$sql = <<<EOD
-  SELECT CODE FROM PROJECTS WHERE CODE = '{$proj_code}' AND ACTIVE=Y
+$active_statement = <<<EOD
+  SELECT CODE FROM PROJECTS WHERE CODE = '{$proj_code}'
 EOD;
-$result = mysql_query($sql,$conn) or die("Query Error: " . mysql_error());
+$closed_statement = <<<EOD
+  SELECT CODE FROM PROJECTS WHERE CODE = '{$proj_code}'
+EOD;
 
-if ($result)
+$active_query = mysql_query($active_statement,$conn) or die("Query Error: " . mysql_error());
+
+$is_active = mysql_fetch_array($active_query); 
+
+$closed_query = mysql_query($closed_statement,$conn) or die("Query Error: " . mysql_error());
+
+$is_closed = mysql_fetch_array($closed_query); 
+
+if ($is_active['CODE'])
 {
-  $values = mysql_fetch_array($result);
-  print $values['CODE'];
+	print 'EXISTS'; 
 }
+else if ($is_closed['CODE'])
+{
+	print 'CLOSED'; 
+}
+
+else {
+	print 'NOEXIST';
+}
+
 
 ?>
