5a43b47b92cd6a22939df04ac84f30ecfef65c22
Alex Ramsdell <alexramsdell@gmail.com>
2015-08-26 16:37:51 -0700

    rearranged project directory for production

--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-scp {components.html,sg-style.css} alexr@gate.nmr.mgh.harvard.edu:/homes/2/alexr/public_html/pub/pb/
+scp -r {style_guide} alexr@gate.nmr.mgh.harvard.edu:/homes/2/alexr/public_html/pub/pb/
