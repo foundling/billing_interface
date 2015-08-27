7ae9b1218c0619616ee86ec23d65afd047d3563a
Alex Ramsdell <alexramsdell@gmail.com>
2015-08-27 16:19:53 -0700

    submission errors available now in the UI

--- common/js/modules/form.js
+++ common/js/modules/form.js
@@ -4,19 +4,26 @@ var Form = function(form_modules) {
         irv, // intermediate return value, for clarity
         rv = true,
         // cache dom
-        $submit_btn = $('input[type="submit"]');
+        $submit_btn = $('input[type="submit"]'),
+        $error_box = $('#submission-error-box'),
+        $error_list = $('#error-list'),
+        errors = [];
+
 
     // bind events
     $submit_btn.on('click', validate);
 
     function validate(e) {
-        
-        // IF RV is TRUE: FORM IS VALID
-        
+
+        clearErrors(); 
+
+        // NOTE: IF RV is TRUE: FORM IS VALID
+
         for (module in form_modules) {
             if (form_modules[module].validate) {
                 console.log('validating ' + module + '!');
                 irv = form_modules[module].validate();
+                if (!irv) { errors.push(module)}
                 console.log('status for ' + module + ': ' + irv);
                 rv = rv && irv; 
             }
@@ -24,11 +31,28 @@ var Form = function(form_modules) {
         console.log('validation result: ' + rv + "\n" + "\n");
 
         if (!rv) {
-            rv = true;
             e.preventDefault();
+            report();
+            rv = true; // resetting rv to default as true for subsequent attempts
         } 
+    }
 
-       
+    function report() {
+        $error_box.removeClass('hidden');
+        $error_box.addClass('active-errors');
+
+        errors.forEach( function(value, index) {
+            var $li = $('<li>' + value.replace('_',' ') + '</li>');
+            $error_list.append($li);
+        });
+    }
+
+    function clearErrors() {
+        $error_box.addClass('hidden');
+        $error_box.removeClass('active-errors');
+
+        errors = [];
+        $error_list.empty();
     }
 
 }
